fd05fd90d5ff2300b3abfd02f38080febc4abb7f
gs <guidosalem.dev@gmail.com>
2024-03-19 18:10:26 -0300

    first commit

---
+++ App.svelte
@@ -0,0 +1,28 @@
+<!-- Script JS -->
+<script>
+</script>
+
+<!-- Estructura contenido HTML -->
+<br>
+<h1 id="titulo">Línea D</h1>
+<h3 id="subt">Porcentaje del Trayecto Realizado</h3>
+<div class="graficos">
+    <div class="caja1">
+        <p id="par1">Congreso de Tucumán</p>
+        <img class="imagenes" src="./images/Progreso.png" alt="Progreso de Norma en su trayecto"/>
+        <br>
+        <p id="par2">Catedral</p>
+        <p class="epigrafe">25% del viaje</p>
+    </div>
+    <div class="caja2">
+        <p id="par1">Congreso de Tucumán</p>
+        <img class="imagenes" src="./images/Linea D.png" alt="Trayecto completo"/>
+        <br>
+        <p id="par2">Catedral</p>
+        <p class="epigrafe">100% del viaje</p>
+    </div>
+</div>
+
+<!-- Estilos CSS -->
+<style>
+</style>
